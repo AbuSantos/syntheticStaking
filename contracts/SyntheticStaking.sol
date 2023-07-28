044adc19dc219fe08e765b5155ecf68a3189cad6
Abusantos <abusomwansantos@gmail.com>
2023-07-28 13:00:59 +0100

    updates

--- contracts/SyntheticStaking.sol
+++ contracts/SyntheticStaking.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 import "./IERC20.sol";
 
 contract SyntheticStaking {
-    IERC20 public rewardToken;
+    IERC20 public rewardsToken;
     IERC20 public stakingToken;
 
     uint public rewardRate = 100;
@@ -11,6 +11,39 @@ contract SyntheticStaking {
     uint public rewardPerTokenStored;
 
     mapping(address => uint) public userRewardPerTokenPaid;
-    mapping(address =>uint) public reward;
-    constructor() {}
+    mapping(address => uint) public reward;
+
+    uint private _totalSupply;
+    mapping(address => uint) private _balances;
+
+    constructor(address _rewardsToken, address _stakingToken) {
+        rewardsToken = IERC20(_rewardsToken);
+        stakingToken = IERC20(_stakingToken);
+    }
+
+    modifier updateReward(address account) {
+        _;
+    }
+
+    function rewardPerToken() public view returns (uint) {}
+
+    function earned(address account) public view returns (uint) {}
+
+    function stake(uint _amount) external updateReward(msg.sender) {
+        _totalSupply += _amount;
+        _balances[msg.sender] += _amount;
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint _amount) external updateReward(msg.sender) {
+        _totalSupply -= _amount;
+        _balances[msg.sender] -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
+    }
+
+    function getReward() external updateReward(msg.sender) {
+        uint rewards = reward[msg.sender];
+        reward[msg.sender] = 0;
+        rewardsToken.transfer(msg.sender, rewards);
+    }
 }
